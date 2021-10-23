bb37e5b9a5f38e6469659b9449c7f67d24ccbabd
trinhnhathuu <schoolgame2002@gmail.com>
2021-10-23 16:02:06 +0700

    first commit

---
+++ 21_10.js
@@ -0,0 +1,9 @@
+
+/*document.getElementById("more").onclick = function () {
+    document.getElementById("content").style.display = 'none';
+};
+
+document.getElementById("an").onclick = function () {
+    document.getElementById("content").style.display = 'block';
+};
+*/
